f1c0c3515b7b7672d525483c517cb7ac9d9f949d
Matko <matkobusljeta@gmail.com>
2020-03-20 22:56:25 +0100

    Added new version of gatsby page

---
+++ component---src-pages-about-js-30816c4f87e14f1da9fa.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"3XHS":function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),o=a("Wbzz"),u=a("Bl7J"),c=a("vrFN");t.default=function(){return l.a.createElement(u.a,null,l.a.createElement(c.a,{title:"About"}),l.a.createElement("h1",null,"About page"),l.a.createElement("p",null,"This is the about page"),l.a.createElement(o.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-30816c4f87e14f1da9fa.js.map
